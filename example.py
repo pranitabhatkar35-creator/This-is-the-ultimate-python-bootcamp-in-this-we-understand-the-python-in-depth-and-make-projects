b7571b64a8528b3a8436ed349dd691f967df1244
student P <pranitabhatkar35@gmil.com>
2025-12-28 06:26:44 +0530

    version 01 system updated

---
+++ example.py
@@ -0,0 +1,44 @@
+ 
+
+
+
+# # # # # # a=int(input("Enter first number:"))
+# # # # # # b=int(input("Enter second number:"))
+# # # # # # if a>b:
+# # # # # #     print(f"{a} is greater than {b}")
+# # # # # # else:
+# # # # # #     print(f"{b} is greater than {a}")
+
+# # # # # num=int(input("Enter a number:"))
+# # # # # if num%2==0:
+# # # # #     print(f"{num} is even")
+# # # # # else:
+# # # # #     print(f"{num} is odd")
+
+
+
+# # # # name=input("Enter your namr:")
+# # # # age=int(input("Enter your age:"))
+# # # # if(age>=18):
+# # # #     print(f"hello {name} you are eligible for vote")
+# # # # else:print(f"hello {name} you are not eligible for vote")
+
+
+
+# # # pizzaSlice=10
+# # # totalPizzaValue=pizzaSlice*6
+# # # userPizzaSlice=int(input("How many pizza you buy:"))
+# # # print(f"totalprice is {userPizzaSlice*totalPizzaValue}")
+
+# # lenths=5.0
+# # width=3.0
+# # area=lenths*width
+# # peremeter=2*(lenths+width)
+# # print(f"{area} sq.units and {peremeter}")
+
+# principal=1000.0
+# rate=5
+# time=3
+# amount=principal*(1+rate/100)**time
+# SimpleInterset=amount-principal
+# print(f"Amount is {SimpleInterset}")
